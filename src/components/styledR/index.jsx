a64fe58fc917e23207c70065f0cdde0debd24859
tylim <limteikyee@gmail.com>
2020-02-07 18:07:25 +0800

    update

--- src/components/styledR/index.jsx
+++ src/components/styledR/index.jsx
@@ -1,1 +1,75 @@
+import styled, { css } from 'styled-components'
+import React from 'react'
 
+const responsiveCssGenerator = config => {
+	const { mapping, min, sLevel } = config
+
+	const cssR = (styledCss = '') => {
+		const type = typeof styledCss
+		if (type === 'string' || Array.isArray(styledCss)) {
+			return styledCss
+		} else if (type === 'object' && styledCss) {
+			let cssString = []
+			for (const prop in styledCss) {
+				if (styledCss[prop] !== undefined && mapping[prop] !== undefined) {
+					cssString = [
+						...cssString,
+						`
+			@media (${min ? 'min' : 'max'}-width: ${mapping[prop]}px) {`,
+						styledCss[prop],
+						`
+		}
+			`,
+					]
+				}
+			}
+			return cssString
+		} else {
+			return ''
+		}
+	}
+
+	const isComponentHtml = component =>
+		typeof component === 'string' ? styled[component] : styled(component)
+
+	const specificityWrapper = (styledCss = '', level = sLevel) => css`
+		${'&'.repeat(level)} {
+			${cssR(styledCss)}
+		}
+	`
+
+	const styledR = comp => (styledCss = '', level = sLevel) => {
+		return isComponentHtml(comp)`
+	 ${specificityWrapper(styledCss, level)}
+	 `
+	}
+	const styledHOC = Comp => (level = sLevel) => {
+		return styled(CompNew)`
+			${props => {
+				const { styledCss } = props
+				return specificityWrapper(styledCss, level)
+			}}
+		`
+	}
+
+	return { cssR, styledR, styledHOC }
+}
+
+const mapping = {
+	xs: 0,
+	sx: 500,
+	sm: 576,
+	md: 768,
+	lg: 992,
+	xl: 1200,
+}
+
+const config = {
+	mapping,
+	min: true,
+	sLevel: 3,
+}
+
+const { cssR, styledR, styledHOC } = responsiveCssGenerator(config)
+
+export { cssR, styledR, styledHOC, css }
--- src/index.jsx
+++ src/index.jsx
@@ -1,55 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Link from './components/Link'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { Helmet } from 'react-helmet'
 
 const Demo = () => {
-	return (
-		<BrowserRouter basename={'/'}>
-			<div>
-				<h1>react-router-link-nostack Demo</h1>
-				<Link to='/'>to index</Link>
-				<br />
-				<Link to='/profile'>to profile</Link>
-			</div>
-			<br />
-			<Switch>
-				<Route
-					exact
-					path='/'
-					render={() => {
-						return (
-							<>
-								<Helmet>
-									<title>Index</title>
-								</Helmet>
-								<p>Now at Index Page(route: '/')</p>
-							</>
-						)
-					}}
-				/>
-				<Route
-					exact
-					path='/profile'
-					render={() => {
-						return (
-							<>
-								<Helmet>
-									<title>Profile</title>
-								</Helmet>
-								<p>Now at Profile Page(route: '/profile')</p>
-							</>
-						)
-					}}
-				/>
-			</Switch>
-			<p>
-				try to click the same route multiple time and see it wont add to history
-				stack!
-			</p>
-		</BrowserRouter>
-	)
+	return <div></div>
 }
 
 render(<Demo />, document.getElementById('root'))
